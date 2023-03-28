14275a7b5e230a6ea036c56b1a88fded118f2007
AntonLTG <93368256+AntonLTG@users.noreply.github.com>
2023-03-28 12:50:36 +0200

    the commits of all commits

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import MainCard from "./lib/MainCard.svelte";
+</script>
+
+<main>
+<MainCard/>
+
+</main>
+
+<style>
+</style>
---
+++ src/lib/MainCard.svelte
@@ -0,0 +1,61 @@
+<div class="mainCard">
+    <div class="Title">
+        Loonie Hoodie
+    </div>
+    <div class="mainRuta">
+        <div class="vRuta">
+            <img src="assets\img\blankethoodie.png" alt="fuckshitup">
+        </div>
+        <div class="hRuta">
+            a
+        </div>
+    </div>
+    
+</div>
+
+<style>
+.mainCard{
+    height: 80vh;
+    width: 80vw;
+    display: flex;
+    flex-direction: column;
+    border-radius: 8vh;
+    background-color: white;
+}
+
+.mainRuta{
+    display: flex;
+    flex-direction: row;
+    height: 90%;
+}
+
+.Title{
+    display: flex;
+    justify-content: center;
+    height: 10%;
+    font-size: 8vh;
+}
+
+.Rutor{
+}
+
+.vRuta, .hRuta{
+    width: 50%;
+}
+
+.vRuta{
+}
+
+.vRuta img{
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+    border-bottom-left-radius: 8vh;
+}
+
+/*{
+    border-style: solid;
+    border-color: red;
+    border-width: 4px;
+}*/
+</style>
